af5d65c9c5b30275f375b028506607feb0c58cd7
vdm-julieke <124454367+vdm-julieke@users.noreply.github.com>
2023-04-21 09:55:00 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left) && Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
+        basic.showNumber(1)
+    }
+    basic.showNumber(0)
+})
